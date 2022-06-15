c7b487bb557eba41e65dcff664c82d517b131114
Justin Hale <drjustinhale@gmail.com>
2022-06-15 21:47:23 +0100

    Add empty di

---
+++ Cygnet/ServiceExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Cygnet;
+
+/// <summary>Extensions to the IServiceCollection to configure the mvc pipeline.</summary>
+public static class ServiceExtensions {
+
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services) {
+        return services;
+    }
+
+}
