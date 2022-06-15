d9402db0f1f37ee2e358b01fb9c3f9d105584fc3
Justin Hale <drjustinhale@gmail.com>
2022-06-15 21:35:36 +0100

    Add mvc pipeline

--- Cygnet/Program.cs
+++ Cygnet/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+    app.UseExceptionHandler("/Error");
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 app.Run();
