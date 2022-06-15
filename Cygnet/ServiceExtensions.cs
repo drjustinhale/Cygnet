aa6d0d18217fe830b34e8597bb66f22be4059bed
Justin Hale <drjustinhale@gmail.com>
2022-06-15 22:25:11 +0100

    Configure di to inject different instances based on query string

--- Cygnet/ServiceExtensions.cs
+++ Cygnet/ServiceExtensions.cs
@@ -7,9 +7,21 @@ public static class ServiceExtensions {
 
     public static IServiceCollection AddDependencyInjection(this IServiceCollection services) {
         services.AddHttpContextAccessor();
-        services.AddTransient<IGreeting, DefaultGreeting>();
-
+        services.AddSingleton<DefaultGreeting>();
+        services.AddSingleton<QueryStringGreeting>();
+        services.AddScoped(AddDependencyInjectionBasedOnQueryString);
         return services;
+    }
+
+    /// <summary>Configure dependency injection to inject different configs based on the presence of a query string.</summary>
+    private static IGreeting AddDependencyInjectionBasedOnQueryString(IServiceProvider provider) {
+        var context = provider.GetRequiredService<IHttpContextAccessor>();
+
+        var hasQueryString = context.HttpContext?.Request.Query.Count != 0;
+
+        if (hasQueryString)
+            return provider.GetRequiredService<QueryStringGreeting>();
+        return provider.GetRequiredService<DefaultGreeting>();
     }
 
 }
