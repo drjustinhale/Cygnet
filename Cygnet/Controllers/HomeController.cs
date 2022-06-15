7510aae369f93f9bb6dee5f57f2ea3bbb7c430a8
Justin Hale <drjustinhale@gmail.com>
2022-06-15 22:20:17 +0100

    Configure di to use default greeting

--- Cygnet/Controllers/HomeController.cs
+++ Cygnet/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
 ﻿namespace Cygnet.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using Models;
 
 public class HomeController : Controller {
 
+    #region Constructors
+
+    private readonly IGreeting _greeting;
+
+    /// <summary>Use dependency injection to provide instance of IGreeting.</summary>
+    public HomeController(IGreeting greeting) {
+        _greeting = greeting;
+    }
+
+    #endregion
+
     [HttpGet]
     public async Task Index() {
-        await Response.WriteAsync("Hello, World!");
+        await Response.WriteAsync(_greeting.Greet);
     }
 
 }
--- Cygnet/ServiceExtensions.cs
+++ Cygnet/ServiceExtensions.cs
@@ -1,9 +1,14 @@
 ﻿namespace Cygnet;
 
+using Models;
+
 /// <summary>Extensions to the IServiceCollection to configure the mvc pipeline.</summary>
 public static class ServiceExtensions {
 
     public static IServiceCollection AddDependencyInjection(this IServiceCollection services) {
+        services.AddHttpContextAccessor();
+        services.AddTransient<IGreeting, DefaultGreeting>();
+
         return services;
     }
 
